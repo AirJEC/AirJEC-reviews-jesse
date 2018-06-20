c2989f0c2b459f4efbd0c09c0a9ce109315ad751
Jesse Chua <13chuajesse@gmail.com>
2018-06-19 17:08:45 -0700

    Copied service repo

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  output: {
+    filename: 'reviewBundle.js',
+    path: __dirname + '/public/dist'
+  }
+};
