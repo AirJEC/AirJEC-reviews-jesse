a4d8d00974f7e7f980509ad34fc1714e02dd491c
Jesse Chua <13chuajesse@gmail.com>
2018-06-27 10:45:59 -0700

    Change query to reflect correct column names

--- database/postgres.js
+++ database/postgres.js
@@ -9,13 +9,13 @@ client.connect((err) => {
   if (err) {
     console.log('Failed to connect to airfec_reviews database.');
   } else {
-    console.log('Connected to airfec_reviews PostgreSQL database.')
+    console.log('Connected to airfec_reviews PostgreSQL database.');
   }
 });
 
 const getData = (id, callback) => {
   client.query(`
-  SELECT reviews.id, reviews.body, reviews.accuracy_rating, reviews.communication_rating, reviews.cleanliness_rating, reviews.location_rating, reviews.checkin_rating, reviews.value_rating, reviews.date_submitted, users.username, users.user_img, listings.host_name, listings.host_photo, listings.host_text 
+  SELECT reviews.id, reviews.review_text, reviews.accuracy_rating, reviews.communication_rating, reviews.cleanliness_rating, reviews.location_rating, reviews.checkin_rating, reviews.value_rating, reviews.review_date, users.guest_name, users.guest_photo, listings.host_name, listings.host_photo, listings.host_text 
   FROM reviews 
   INNER JOIN users ON users.id = reviews.user_id AND reviews.listing_id = ${id} 
   INNER JOIN listings ON reviews.listing_id = listings.id`, (err, res) => {
