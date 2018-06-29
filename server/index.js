e2f55a833428ad5df3836ba8a82880fb5ba6a63c
Jesse Chua <13chuajesse@gmail.com>
2018-06-28 19:16:07 -0700

    Add clustering to index.js

--- server/index.js
+++ server/index.js
@@ -1,64 +1,81 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
 const bodyParser = require('body-parser');
 const db = require('../database/postgres');
 
-const app = express();
-const port = process.env.PORT || 3001;
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,application/xml');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-app.use('/rooms/:id', express.static(path.join(__dirname, '../public/dist/')));
-
-app.get('/reviews/:id', (req, res) => {
-  db.getData(req.params.id, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send(data);
-    }
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
   });
-});
+} else {
+  const app = express();
+  const port = process.env.PORT || 3001;
 
-app.post('/reviews/:id', (req, res) => {
-  db.postData(req.body, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201).send(data);
-    }
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,application/xml');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
   });
-});
 
-app.put('/reviews/:id', (req, res) => {
-  db.putData(req.params.id, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(204).send(data);
-    }
+  app.use('/rooms/:id', express.static(path.join(__dirname, '../public/dist/')));
+
+  app.get('/reviews/:id', (req, res) => {
+    db.getData(req.params.id, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
   });
-});
 
-app.delete('/reviews/:id', (req, res) => {
-  db.deleteData(req.params.id, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(204).send(data);
-    }
+  app.post('/reviews/:id', (req, res) => {
+    db.postData(req.body, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(201).send(data);
+      }
+    });
   });
-});
 
-app.listen(port, () => {
-  console.log('server listening on port ', port);
-});
+  app.put('/reviews/:id', (req, res) => {
+    db.putData(req.body, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(204).send(data);
+      }
+    });
+  });
+
+  app.delete('/reviews/:id', (req, res) => {
+    db.deleteData(req.body.id, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(204).send(data);
+      }
+    });
+  });
+
+  app.listen(port, () => {
+    console.log('server listening on port ', port);
+  });
+}
